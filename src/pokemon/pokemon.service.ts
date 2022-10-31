f41f72f498a381458baab6f85c5122c472d42a43
Esteban Canteros <estebanacanteros@gmail.com>
2022-10-31 14:58:20 -0300

    update for performance on delete

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -55,7 +55,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0) {
+      throw new NotFoundException(`There is no pokemon with id "${id}"`);
+    }
+    return;
   }
 }
