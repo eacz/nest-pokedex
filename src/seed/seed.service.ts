6ce2994f1c1111b821a47f6b66e0efe7953f8299
Esteban Canteros <estebanacanteros@gmail.com>
2022-11-03 12:13:00 -0300

    add adapter pattern for http requests

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import axios, { AxiosInstance } from 'axios';
 
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-    //if(process.env)
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=386',
     );
 
